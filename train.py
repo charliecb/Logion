a3964859e8a0efbcdbd6f7774e1cd3c9646353e5
charliecb <35007252+charliecb@users.noreply.github.com>
2023-03-05 15:00:56 +0000

    Updating training and beam search files

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 import getpass
 import sys
 
-epochs = 80
+epochs = 100
 #training_portion = 0.9      # Reserve 10% of dataset for testing
 #random_seed = 42            # This seed is used so that we can recover the exact train/test split after this script terminates.
 num_logs = 100            # (in steps)
@@ -11,8 +11,8 @@ num_evals = 100          # (in steps)
 num_saves = 10
 mask_proportion = 0.15
 
-main_directory = 'Greek'    # This should be located inside the scratch work drive, /scratch/gpfs/<username>/
-#data_subdirectory = 'data'
+model_directory = 'models/base'     # This folder should be populated with config.json and pytorch_model.bin files. These can be taken from the Dropbox link given in README.md.
+data_directory = 'data'             # Currently empty. Should be populated with two files, train.txt and val.txt, of unicode Greek separated into lines of at most 512 tokens.
 
 if len(sys.argv) == 1:
         print("Received no argument for batch size. Defaulting to 16.")
@@ -21,21 +21,23 @@ elif len(sys.argv) > 1:
         print(f"Setting batch size to {sys.argv[1]}.")
         batch_size = int(sys.argv[1])
 
-num_steps = int(35396/batch_size)
+num_steps = int(35396/batch_size) # This constant is dataset specific and should be modified if the training dataset is changed. It represents the total number of stochastic gradient descent steps taken during training.
 log_every = int(num_steps/num_logs)
 eval_every = int(num_steps/num_evals)
 save_every = int(num_steps/num_saves)
 
-username = getpass.getuser()
-filestem = '/scratch/gpfs/' + username + '/' + main_directory
+#username = getpass.getuser() - Obsolete
+filestem = main_directory
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"Using device {device}.")
 
+# Download Greek tokenizer from Lefever et al., which is borrowed from
+#  the modern Greek tokenizer from Koutsikakis et al. (https://huggingface.co/nlpaueb/bert-base-greek-uncased-v1)
 tokenizer = BertTokenizer.from_pretrained('pranaydeeps/Ancient-Greek-BERT')
-model = BertForMaskedLM.from_pretrained('pranaydeeps/Ancient-Greek-BERT').to(device)
+model = BertForMaskedLM.from_pretrained(model_directory).to(device)
 
-with open('ps_train.txt', 'r') as f:
+with open(data_directory + '/train.txt', 'r') as f:
   text = f.read().split('\n')
   text.pop(-1)
   f.close()
@@ -43,7 +45,7 @@ with open('ps_train.txt', 'r') as f:
 train_inputs = tokenizer(text, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
 train_inputs['labels'] = train_inputs.input_ids.detach().clone()
 
-with open('ps_val.txt', 'r') as f:
+with open(data_directory + '/val.txt', 'r') as f:
   text_val = f.read().split('\n')
   text_val.pop(-1)
   f.close()
@@ -69,7 +71,7 @@ training_args = TrainingArguments(
     eval_steps=eval_every,
     logging_steps=log_every,
     save_steps=save_every,
-    output_dir=filestem + '/out' + str(batch_size),
+    output_dir='out' + str(batch_size),
     per_device_train_batch_size=batch_size,
     num_train_epochs=epochs
 )
@@ -84,4 +86,4 @@ trainer = Trainer(
 
 trainer.train()
 
-model.save_pretrained(filestem + '/content' + str(batch_size) + '/tester')
+model.save_pretrained('content' + str(batch_size) + '/tester')
